60589984a4a13642374313f8b6b238a482e67c20
liuyangming <liuyangming@gmail.com>
2018-12-17 12:05:15 +0800

    提升版本至0.5.0-ALPHA9

--- dubbo-sample/bytejta-sample-consumer/src/main/java/com/bytesvc/service/impl/GenericTransferServiceImpl.java
+++ dubbo-sample/bytejta-sample-consumer/src/main/java/com/bytesvc/service/impl/GenericTransferServiceImpl.java
@@ -2,20 +2,20 @@ package com.bytesvc.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.dubbo.config.annotation.Service;
 import com.bytesvc.ServiceException;
 import com.bytesvc.service.IAccountService;
 import com.bytesvc.service.ITransferService;
 
-@Service("genericTransferService")
+@Service(interfaceClass = ITransferService.class, group = "x-bytejta", loadbalance = "bytejta", filter = "bytejta", cluster = "failfast", retries = 0)
 public class GenericTransferServiceImpl implements ITransferService {
 
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
-	@Reference(interfaceClass = IAccountService.class, group = "x-bytejta", loadbalance = "bytejta", filter = "bytejta", cluster = "failfast", retries = 0)
+	@Reference(interfaceClass = IAccountService.class, group = "x-bytejta", loadbalance = "bytejta", filter = "bytejta", cluster = "failfast", retries = 0, timeout = 15000)
 	private IAccountService remoteAccountService;
 
 	@Transactional(rollbackFor = ServiceException.class)
@@ -25,10 +25,7 @@ public class GenericTransferServiceImpl implements ITransferService {
 	}
 
 	private void increaseAmount(String acctId, double amount) throws ServiceException {
-		int value = this.jdbcTemplate.update("update tb_account_two set amount = amount + ? where acct_id = ?", amount, acctId);
-		if (value != 1) {
-			throw new ServiceException("ERROR!");
-		}
+		this.jdbcTemplate.update("update tb_account_two set amount = amount + ? where acct_id = ?", amount, acctId);
 		System.out.printf("exec increase: acct= %s, amount= %7.2f%n", acctId, amount);
 	}
 
--- dubbo-sample/bytejta-sample-provider/src/main/java/com/bytesvc/service/impl/AccountServiceImpl.java
+++ dubbo-sample/bytejta-sample-provider/src/main/java/com/bytesvc/service/impl/AccountServiceImpl.java
@@ -16,19 +16,13 @@ public class AccountServiceImpl implements IAccountService {
 
 	@Transactional(rollbackFor = ServiceException.class)
 	public void increaseAmount(String acctId, double amount) throws ServiceException {
-		int value = this.jdbcTemplate.update("update tb_account_one set amount = amount + ? where acct_id = ?", amount, acctId);
-		if (value != 1) {
-			throw new ServiceException("ERROR!");
-		}
+		this.jdbcTemplate.update("update tb_account_one set amount = amount + ? where acct_id = ?", amount, acctId);
 		System.out.printf("exec increase: acct= %s, amount= %7.2f%n", acctId, amount);
 	}
 
 	@Transactional(rollbackFor = ServiceException.class)
 	public void decreaseAmount(String acctId, double amount) throws ServiceException {
-		int value = this.jdbcTemplate.update("update tb_account_one set amount = amount - ? where acct_id = ?", amount, acctId);
-		if (value != 1) {
-			throw new ServiceException("ERROR!");
-		}
+		this.jdbcTemplate.update("update tb_account_one set amount = amount - ? where acct_id = ?", amount, acctId);
 		System.out.printf("exec decrease: acct= %s, amount= %7.2f%n", acctId, amount);
 	}
 
